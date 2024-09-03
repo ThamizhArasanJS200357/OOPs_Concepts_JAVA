d02fc15db347a7814c3820d62b9a2741b56e1abe
Thamizh Arasan JS <146479516+ThamizhArasanJS200357@users.noreply.github.com>
2024-09-03 21:57:08 +0530

    Add files via upload

---
+++ Class and Object/Class.java
@@ -0,0 +1,31 @@
+
+class Class1{
+    String name;
+    int rollno;
+
+    public void display(){
+        System.out.println(name);
+        System.out.println(rollno);
+    }
+    public void setvalue(String s,int t){
+        name=s;
+        rollno=t;
+    }
+}
+
+public class Class{
+    public static void main(String[] args) {
+        Class1 obj=new Class1();
+        obj.name="Rolex";
+        obj.rollno=57;
+        obj.display();
+
+        Class1 js=new Class1();
+        js.name="thamizh";
+        js.rollno=44;
+        js.display();
+
+        obj.setvalue("JS", 12);
+        obj.display();
+    }
+}
---
+++ Class and Object/Object.java
@@ -0,0 +1,16 @@
+public class Object{
+    String name;
+    int rollno;
+
+    public void display(){
+        System.out.println(name);
+        System.out.println(rollno);
+    }
+
+    public static void main(String[] args) {
+        Object obj=new Object();
+        obj.name="Rolex";
+        obj.rollno=57;
+        obj.display();
+    }
+}
