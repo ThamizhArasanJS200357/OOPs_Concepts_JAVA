4a963a6ec5c0b14cfc42d2eaf30aa320a6e3ca66
Thamizh Arasan JS <146479516+ThamizhArasanJS200357@users.noreply.github.com>
2024-09-03 21:49:17 +0530

    Add files via upload

---
+++ Recursion/Recursion.java
@@ -0,0 +1,15 @@
+
+public class Recursion {
+    public static void main(String[]args){
+         naturalprint(10);
+    }
+    public static void naturalprint(int s){
+        if(s==1){
+            System.out.println(1);
+        }
+        else{
+            System.out.println(s);
+            naturalprint(s-1);
+        }
+    }
+}
